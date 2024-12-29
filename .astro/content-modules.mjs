c12713994791df55a31077c0b4b8d7ee35730710
johndecosta <john@eaglefox.net>
2024-12-29 11:58:48 +1000

    add remaining photos folders to project folder src

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,1 @@
-
-export default new Map([
-["src/content/posts/victoria-2024-great-ocean-road-prt2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fvictoria-2024-great-ocean-road-prt2.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
